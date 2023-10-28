48c51ef7d3d7b72eb198f6f3674172adbaee9a2c
KrunalJadav <132035320+KrunalJadav@users.noreply.github.com>
2023-10-28 15:38:58 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
-// import logo from './logo.svg';
-// import './App.css';
+import React from "react";
+import Covid from "./component/Covid";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-// App.js 
-
-import React from 'react'; 
-import FlipCoin from './components/FlipCoin'
-
-const App=()=> { 
-return ( 
-	<div className="App"> 
-	<FlipCoin /> 
-	</div> 
-); 
-} 
+function App() {
+  return (
+<>
+<Covid />
+</>
+  );
+}
 
 export default App;
